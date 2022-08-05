e023e5242aefc5ec4cb075fce6b18f7c3f82551e
JoshiNagendra <109235351+JoshiNagendra@users.noreply.github.com>
2022-08-05 22:30:37 +0530

    Add files via upload

---
+++ GuessingGame.java
@@ -0,0 +1,166 @@
+package Game;
+
+import java.util.Scanner;
+class Guesser{
+	//Guesser will guess the number
+	public int Guesser(){
+		Scanner sc = new Scanner(System.in);
+		int Guessnumber;
+		System.out.print("Guesser Kindly Enter the number from range 0 to 50 :");
+		Guessnumber=sc.nextInt();
+		return Guessnumber;
+	}
+}
+class Player{
+	//Players will enter their answers
+	public int guessNumber(){
+	Scanner sc = new Scanner(System.in);
+	int playerGuessno;
+	System.out.print("Player kindly guess the number from range 0 to 50  :");
+	playerGuessno = sc.nextInt();
+	return playerGuessno;
+	}	
+}
+class Umphire{
+	int numFromGuesser;
+	int numFromPlayer1;
+	int numFromPlayer2;
+	int numFromPlayer3;
+	//Umpire will collect the number from Guesser
+	public void collectnumberFromGuesser(){
+		Guesser gs = new Guesser();
+		numFromGuesser=gs.Guesser();
+	}
+	//Umpire will collect the number from players
+	public void collectnumberFromPlayers(){
+		Player p1=new Player();
+		Player p2=new Player();
+		Player p3=new Player();
+		numFromPlayer1=p1.guessNumber();
+		numFromPlayer2=p2.guessNumber();
+		numFromPlayer3=p3.guessNumber();
+	}
+	//Umpire will compare the numbers of players and the guesser's number
+	public void compare(){
+		System.out.println();
+    //1st Condition
+	if(numFromGuesser<=50) 
+	{
+		if(numFromPlayer1==numFromGuesser) 
+		{
+			if(numFromPlayer1==numFromPlayer2&&numFromPlayer1==numFromPlayer3)
+			{
+				System.out.println("Match got  tied");
+			}
+			else if(numFromPlayer1==numFromPlayer2) 
+			{
+				System.out.println("1 and 2 Players Won the game");
+			}
+			else if(numFromPlayer1==numFromPlayer3) 
+			{
+				System.out.println(" 1 and 3 Players won the game");
+			}else 
+			{
+			System.out.println("1st Player won the Game");
+			}
+		}
+		else if(numFromPlayer2==numFromGuesser) 
+		{
+			if(numFromPlayer2==numFromPlayer3)
+			{
+				System.out.println("2 and 3rd Players won the game");
+			}
+			else if(numFromPlayer2!=numFromGuesser) {
+				System.out.println("2nd Player Better Luck Next Time");
+			}
+			else 
+			{
+			System.out.println("2nd Player won the game");
+			}
+		}
+		else if(numFromPlayer3==numFromGuesser)
+		{
+			System.out.println("3rd Player won the game");
+		}
+		else
+		{
+			System.out.println("All Lost Please Try Again");
+		}
+		//2nd condition
+		if(numFromGuesser!=numFromPlayer1&&numFromGuesser!=numFromPlayer2&&numFromGuesser!=numFromPlayer3)
+		{
+			System.out.println();
+			System.out.println("~~~~~All Players - Better Luck Next Time !!~~~~~");
+		}
+		else if(numFromGuesser!=numFromPlayer1&&numFromGuesser!=numFromPlayer2) 
+		{
+			System.out.println();
+			System.out.println("~~~~~1st and 2nd player - Better luck next time !!~~~~~");
+		}
+		else if(numFromGuesser!=numFromPlayer1&&numFromGuesser!=numFromPlayer3) 
+		{
+			System.out.println();
+			System.out.println("~~~~~1st and 3rd player - Better luck next time !!~~~~~");
+		}else if(numFromGuesser!=numFromPlayer2&&numFromGuesser!=numFromPlayer3) 
+		{
+			System.out.println();
+			System.out.println("~~~~~2nd and 3rd Players - Better Luck Next Time !!~~~~~");
+		}
+		else if(numFromGuesser!=numFromPlayer1) 
+		{
+			System.out.println();
+			System.out.println("~~~~~Player 1 - Better Luck next time !!~~~~~");
+		}
+		else if(numFromGuesser!=numFromPlayer2) 
+		{
+			System.out.println();
+			System.out.println("~~~~~Player 2 - Better Luck next time !!~~~~~");
+		}
+		else if(numFromGuesser!=numFromPlayer3) 
+		{
+			System.out.println();
+			System.out.println("~~~~~Player 3 - Better Luck next time !!~~~~~");
+		}
+		
+	//3rd Condition
+	if(numFromPlayer1>50) 
+	{
+		System.out.println();
+		System.out.println("<--**********************************************-->");
+		System.out.println("* Player 1 please guess the number from range 0-50 *");
+		
+	}
+	if(numFromPlayer2>50) 
+	{
+		System.out.println();
+		System.out.println("**********************************");
+		System.out.println("Player 2 please guess the number from range 0-50");
+	}
+	if(numFromPlayer3>50) 
+	{
+		System.out.println();
+		System.out.println("************************************");
+		System.out.println("Player 3 please guess the number from range 0-50");
+	}
+		
+	}
+	else 
+	{
+		System.out.println("Invalid !! Guesser guessed the number above 50 that is not in range");
+		System.out.println();
+		System.out.println("******************************Rematch******************************");
+		collectnumberFromGuesser();
+		collectnumberFromPlayers();
+		compare();
+	}
+  }
+}
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		Umphire ump= new Umphire();
+		ump.collectnumberFromGuesser();
+		ump.collectnumberFromPlayers();
+		ump.compare();
+	}
+}
